3ba711d5bb3bf438bf8e8b58a2a9822ffdf00859
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-02-17 22:42:24 +0200

    feat: add AboutSection component.

---
+++ src/components/organisms/AboutSection.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Title from "../atoms/Title";
+import Text from "../atoms/Text";
+
+export const AboutSection = () => {
+  return (
+    <div className="ds-bg p-7 flex justify-center flex-col items-center">
+      <Title>AboutSection</Title>
+      <Text size="md">
+        This section represents Some information about the website.
+      </Text>
+      <Text size="md" className="text-purple-400">
+        created by : Nada Wael Abdelfattah
+      </Text>
+    </div>
+  );
+};
