f5e99500470ff75e51908ccd8c71d80c8597a90f
ManarTaha-1 <mt9707182@gmail.com>
2026-02-17 22:46:55 +0200

    Add FeatureSection

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,10 @@
 import HeroSections from "@/components/organisms/HeroSections";
+import FeatureSection from "./../../components/organisms/featureSection"
 export default function page() {
   return (
     <div>
       <HeroSections />
+      <FeatureSection/>
     </div>
   );
 }
