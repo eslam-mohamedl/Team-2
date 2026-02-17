4d8e08c36f784f0334712cd9304a2e6f63e82d0e
Mariam Hagras <mariamhagras99@gmail.com>
2026-02-17 22:56:53 +0200

    feat: add ServiceSection to page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,10 @@
 import HeroSections from "@/components/organisms/HeroSections";
+import ServiceSection from "@/components/organisms/ServiceSection";
 export default function page() {
   return (
     <div>
       <HeroSections />
+      <ServiceSection/>
     </div>
   );
 }
