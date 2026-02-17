0788f910ba7b4eb1641661cf2f56c88a5a34a21b
Reem-Amr7 <147060181+Reem-Amr7@users.noreply.github.com>
2026-02-17 22:56:03 +0200

    update add blog section

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,10 @@
 import HeroSection from "@/components/organisms/HeroSection";
+import Blog from "@/components/organisms/Blog";
 export default function page() {
   return (
     <div>
       <HeroSection />
-  
+  <Blog/>
     </div>
   );
 }
