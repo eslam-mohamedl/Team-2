188901722db780edf95bd968a5ae60e2c033bfb0
eslam-mohamedl <ee.eslam355@gmail.com>
2026-02-17 22:23:44 +0200

    update

---
+++ src/components/organisms/HeroSections.tsx
@@ -0,0 +1,15 @@
+import Text from "../atoms/Text";
+import Title from "../atoms/Title";
+export default function HeroSections() {
+  return (
+    <div className="ds-bg p-7 flex justify-center flex-col items-center">
+      <Title>HeroSections</Title>
+      <Text size="md">
+        This section represents the Hero Section of the website.
+      </Text>
+      <Text size="md" className="text-green-400">
+        created by : Eslam Mohamed
+      </Text>
+    </div>
+  );
+}
