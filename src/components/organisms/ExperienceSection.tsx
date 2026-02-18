1a4fd6e29d4b06f93b4b6eacc19f7d1b89b8326f
YARA HASSAN <yarah5612@gmail.com>
2026-02-18 20:37:51 +0200

    feat:add experience section

---
+++ src/components/organisms/ExperienceSection.tsx
@@ -0,0 +1,15 @@
+import Text from "@/components/atoms/Text";
+import Title from "@/components/atoms/Title";
+export default function ExperienceSection() {
+  return (
+    <div className="ds-bg p-7 flex justify-center flex-col items-center">
+      <Title>ExperienceSection</Title>
+      <Text size="md">
+        This section represents the Experience Section of the website.
+      </Text>
+      <Text size="md" className="text-green-400">
+        created by : Yara Hassan
+      </Text>
+    </div>
+  );
+}
