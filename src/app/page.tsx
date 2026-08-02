e1596af99ec772d8adf9d77e3f3655693f627937
Mariem Elgendy <mariem.elgendy213@gmail.com>
2026-08-02 21:24:09 +0300

    feat: Add Pricing Section Structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Herosection from "@/components/Herosection";
 import Aboutsection from "@/components/Aboutseciton";
+import Pricingsection from "@/components/Pricingsection";
+
 export default function page() {
   return (
     <>
       <Herosection />
       <Aboutsection />
+      <Pricingsection />
     </>
   );
 }
