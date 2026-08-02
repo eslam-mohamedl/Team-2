aa43e94f927739f9739b97d73faf863c7192c93c
Your Mahmoud-hamed7 <mh8207977@gmail.com>
2026-08-02 21:42:47 +0300

    feat: add testimonial component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Herosection from "@/components/Herosection";
 import Aboutsection from "@/components/Aboutseciton";
+import Testimonial from "@/components/Testimonial";
 export default function page() {
   return (
     <>
       <Herosection />
       <Aboutsection />
+      <Testimonial/>
     </>
   );
 }
