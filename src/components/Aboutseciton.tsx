fe3745c5f86ef0eee82e46f117aa2fef2e4fdab8
eslam-mohamedl <ee.eslam355@gmail.com>
2026-08-02 22:01:27 +0300

    install lucide react icons

--- src/components/Aboutseciton.tsx
+++ src/components/Aboutseciton.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Check } from "lucide-react";
 
 export default function Aboutsection() {
   return (
     <div className="bg-blue-400 p-5 flex justify-center items-center text-4xl">
       Aboutsection
+      <Check />
     </div>
   );
 }
