bba484e9b629e9d0368a7b67588c0d3a5e92b8fe
eslam-mohamedl <ee.eslam355@gmail.com>
2026-08-02 21:09:17 +0300

    feat: add about section components

---
+++ src/components/Aboutseciton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Aboutsection() {
+  return (
+    <div className="bg-blue-400 p-5 flex justify-center items-center text-4xl">
+      Aboutsection
+    </div>
+  );
+}
