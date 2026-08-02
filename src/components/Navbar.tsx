5a0f523763a912c5924934a8351e8156cd50f3a7
Raghda Mohammed <113856936+Raghda-Mohammed@users.noreply.github.com>
2026-08-03 00:03:49 +0300

    Revert "Add Navbar task"

--- src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Navbar() {
-    return (
-        <div className="bg-blue-500 p-5 flex justify-center items-center text-4xl">
-            Navbar Section
-        </div>
-    );
-}
