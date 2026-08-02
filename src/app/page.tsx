d68457623035469ad3c80d196e8df044ea361e8b
eslam-mohamedl <ee.eslam355@gmail.com>
2026-08-02 21:48:47 +0300

    feat: add team section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Herosection from "@/components/Herosection";
 import Aboutsection from "@/components/Aboutseciton";
+import TeamSection from "@/components/TeamSection";
 export default function page() {
   return (
     <>
       <Herosection />
       <Aboutsection />
+      <TeamSection />
     </>
   );
 }
