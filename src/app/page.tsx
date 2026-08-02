3e605a484ed523a760b66b5a2a5cb393f818fa73
Sama-Ali <samaali2h@gmail.com>
2026-08-02 21:28:44 +0300

    contactSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Herosection from "@/components/Herosection";
 import Aboutsection from "@/components/Aboutseciton";
+import Contact from "@/components/Contact";
 export default function page() {
   return (
     <>
       <Herosection />
       <Aboutsection />
+      <Contact />
     </>
   );
 }
