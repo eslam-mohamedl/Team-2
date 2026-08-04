749551e14d4787dbaa2101dac21a30f6a0799fa4
kokoashraf55555-commits <kokoashraf55555@gmail.com>
2026-08-04 12:51:04 +0300

    feat: add StatisticsSection

---
+++ src/components/StatisticsSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StatisticsSection() {
+  return (
+    <div className="bg-orange-400 p-5 flex justify-center items-center text-4xl">StatisticsSection</div>
+  )
+}
