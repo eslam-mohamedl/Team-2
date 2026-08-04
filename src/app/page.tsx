1513c83dbceb999f462d0ff6ad922c53a102400c
HalaAmir <hala.amir.said@gmail.com>
2026-08-04 19:19:28 +0300

    feat: add ServicesSection components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ import FAQsection from "@/components/FAQsection";
 import Navbar from "@/components/Navbar";
 import StatisticsSection from "@/components/StatisticsSection";
 import CallToActionSection from "@/components/CallToActionSection";
+import ServicesSection from "@/components/ServicesSection";
+
 export default function page() {
   return (
     <>
@@ -25,6 +27,7 @@ export default function page() {
       <Questions/>
       <FAQsection />
       <StatisticsSection/>
+      <ServicesSection/>
     </>
   );
 }
