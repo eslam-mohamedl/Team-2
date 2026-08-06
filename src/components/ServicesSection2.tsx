5cb001ec6bea959a978fea90bf44c6b2e1fd7e34
wwwamrfatouh22-sys <www.amrfatou22@gmail.com>
2026-08-06 18:23:02 +0300

    feat: add ServicesSection2 components

---
+++ src/components/ServicesSection2.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServicesSection2() {
+  return (
+    <div>
+      <i className="bg-gray-900 p-5 flex justify-center items-center text-4xl">Services Section</i>
+    </div>
+  )
+}
+
+export default ServicesSection2
