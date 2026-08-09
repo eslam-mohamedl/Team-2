1b83c1568f9892aa21281a506327e83a161ecdbe
Your Mahmoud-hamed7 <mh8207977@gmail.com>
2026-08-09 22:21:32 +0300

    change color to  hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function HeroSection() {
   return (
-    <div className='bg-black p-5 text-5xl flex justify-center'>HeroSection</div>
+    <div className='bg-gray-700  p-5 text-5xl flex justify-center'>HeroSection</div>
   )
 }
