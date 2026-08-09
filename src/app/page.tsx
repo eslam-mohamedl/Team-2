3941c778eaed64a1566a7398b62de046e62030a2
Raghda Mohammed <raghda191987@gmail.com>
2026-08-09 21:48:40 +0300

    feat: add faq section component with accordion interactivity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import About from "@/components/About";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import FAQSection from "@/components/FAQSection";
 export default function page() {
   return (
     <div>
       <Navbar />
       <About />
       <Footer />
+      <FAQSection />
     </div>
   );
 }
