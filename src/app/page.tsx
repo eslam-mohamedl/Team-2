54cd3d2899aac19d0cc5841d4df9d5558a87a83e
Sama-Ali <samaali2h@gmail.com>
2026-08-09 21:33:39 +0300

    feat: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import About from "@/components/About";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 export default function page() {
   return (
     <div>
       <Navbar />
       <About />
+      <Footer />
     </div>
   );
 }
