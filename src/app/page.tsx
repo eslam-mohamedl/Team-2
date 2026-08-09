011f273ac204a1f9d7e6511e5205ceb7daca2270
Your Mahmoud-hamed7 <mh8207977@gmail.com>
2026-08-09 21:47:24 +0300

    add hero section to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import About from "@/components/About";
 import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
 export default function page() {
   return (
     <div>
       <Navbar />
+      <HeroSection/>
       <About />
     </div>
   );
