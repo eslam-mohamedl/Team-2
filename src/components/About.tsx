2f169362aa60b59a70f035efd9408a3a4a8b232d
Sara Zakarya <sarazakarya8@gmail.com>
2026-08-09 22:14:43 +0300

    feat: Add Style

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div className='bg-blue-400 flex justify-center items-center p-5 text-3xl'>About</div>
-  )
-}
+    <div className="bg-amber-950 flex justify-center items-center p-5 text-3xl">
+      About
+    </div>
+  );
+};
 
-export default About
+export default About;
