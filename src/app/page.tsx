293c555139e30cdf34bceb5bc997727d058886d2
kokoashraf55555-commits <kokoashraf55555@gmail.com>
2026-08-11 21:40:14 +0300

    featCallToAction : add CallToAction component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import FAQSection from "@/components/FAQSection";
 import HeroSection from "@/components/HeroSection";
 import AboutBox from "@/components/AboutBox";
 import Hero from "@/components/Hero";
+import CallToAction from "@/components/CallToAction";
 export default function page() {
   return (
     <div>
       <Hero />
       <AboutBox />
+      < CallToAction/>
     </div>
   );
 }
