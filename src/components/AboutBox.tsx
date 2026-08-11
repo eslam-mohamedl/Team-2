edfefd2092edfc08716056f336e440b610a5aaa4
eslam-mohamedl <ee.eslam355@gmail.com>
2026-08-11 21:22:33 +0300

    style: change bg webite

--- src/components/AboutBox.tsx
+++ src/components/AboutBox.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function AboutBox() {
   return (
-    <div className="bg-blue-600 p-5 text-5xl flex justify-center">AboutBox</div>
+    <div className="bg-green-300 p-5 text-5xl flex justify-center">
+      AboutBox
+    </div>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <div className="bg-red-400 p-5 text-5xl flex justify-center">Hero</div>
+    <div className="bg-green-300 p-5 text-5xl flex justify-center">Hero</div>
   );
 }
