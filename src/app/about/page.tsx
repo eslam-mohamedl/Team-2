d0552a48b67a94fe1cb861e28387e6b6c46a19b0
eslam-mohamedl <ee.eslam355@gmail.com>
2026-08-11 21:48:24 +0300

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import Blog from "@/components/Blog";
 import Feature from "@/components/Feature";
 import Team from "@/components/Team";
+import Contect from "@/components/Contect";
 
 export default function page() {
   return (
     <>
       <div>about</div>
       <Blog />
-       <Feature />
+      <Feature />
       <Team />
+      <Contect />
     </>
-
-);
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,12 @@ import FAQSection from "@/components/FAQSection";
 import HeroSection from "@/components/HeroSection";
 import AboutBox from "@/components/AboutBox";
 import Hero from "@/components/Hero";
-import Contect from "@/components/Contect";
 export default function page() {
   return (
     <div>
       <Hero />
       <AboutBox />
-      <Contect />
+     
     </div>
   );
 }
