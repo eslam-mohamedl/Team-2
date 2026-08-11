1c474e2c271f57b579104d89e30636d55c94ff00
Mariem Elgendy <mariem.elgendy213@gmail.com>
2026-08-11 22:13:52 +0300

    style: change colors f heading

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Statistics() {
     return (
-        <div className='bg-purple-400 flex justify-center items-center p-5 text-3xl'>
-            Statistics Section
+        <div className='bg-purple-400 text-amber-600 flex justify-center items-center p-5 text-4xl'>
+            Statistics
         </div>
     )
 }
