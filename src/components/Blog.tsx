4bc2ad35914bd94d7312fb509b9f286e69296a30
Sama-Ali <samaali2h@gmail.com>
2026-08-11 22:09:49 +0300

    hotfixBlog

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="bg-green-400 flex justify-center items-center p-5 text-3xl">
+    <div className="bg-red-400 flex justify-center items-center p-5 text-3xl">
       About
     </div>
   );
