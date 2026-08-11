7edaaad6f67dff97dcc5105c61e6abd0e1bd7024
Sara Zakarya <sarazakarya8@gmail.com>
2026-08-11 21:39:36 +0300

    Add Team

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Feature from "@/components/Feature";
+import Team from "@/components/Team";
 export default function page() {
   return (
     <div>
       <Feature />
+      <Team />
     </div>
   );
 }
