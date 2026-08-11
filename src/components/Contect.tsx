8d3818c769d3a65dded92c170ee20ebc5f8e513c
wwwamrfatouh22-sys <www.amrfatou22@gmail.com>
2026-08-11 21:44:14 +0300

    feat: add contect components

---
+++ src/components/Contect.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contect = () => {
+  return (
+    <div className='bg-blue-400 flex justify-center items-center p-5 text-3xl'>contect</div>
+  )
+}
+
+export default Contect
