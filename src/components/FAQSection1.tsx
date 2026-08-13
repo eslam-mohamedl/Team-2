b832621995e96a071ee3b25e89d2ae89122fc012
Raghda Mohammed <raghda191987@gmail.com>
2026-08-13 20:37:56 +0300

    add component FAQSection1 and change style component FAQSection

---
+++ src/components/FAQSection1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FAQSection1() {
+  return (
+    <div className='bg-cyan-950 flex justify-center items-center p-5 text-3xl'>
+      FAQ Section 1
+    </div>
+  )
+}
