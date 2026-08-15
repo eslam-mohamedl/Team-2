7bb947f246461a77e164beddc3630aafce72032f
Your Mahmoud-hamed7 <mh8207977@gmail.com>
2026-08-15 03:00:17 +0300

    hotfix change bh

--- src/components/Testmo.tsx
+++ src/components/Testmo.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Testmo() {
   return (
-    <div className='bg-red-950  flex justify-center items-center p-5 text-3xl'>Testmo</div>
+    <div className='bg-blue-100-950  flex justify-center items-center p-5 text-3xl'>Testmo</div>
   )
 }
